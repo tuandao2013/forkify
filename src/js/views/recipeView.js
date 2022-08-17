be747cd194c751167df9265f2ce31cde11005a74
tuandao2013 <daotientuan2013@gmail.com>
2022-08-17 12:44:57 +0700

    remove Fraction add new fraction function in healper.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,8 @@
 import View from './view.js';
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
+import { numberToFraction } from '../helper.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -138,7 +139,7 @@ class RecipeView extends View {
                   <use href="${icons}#icon-check"></use>
                 </svg>
                 <div class="recipe__quantity">${
-                  ing.quantity ? new Fraction(ing.quantity).toString() : ''
+                  ing.quantity ? numberToFraction(ing.quantity).toString() : ''
                 }</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
